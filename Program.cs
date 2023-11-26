c7d19d23a17653e4bc87847c0f42ab697fb91504
GcellyGomes <gicelly.gomes@etec.sp.gov.br>
2023-11-26 20:10:10 -0300

    concluido

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Exercicio EntradaValida GicellyGomes
+﻿Console.WriteLine("--- Entrada Válida ---\n");
+
+int numeroDigitado = -1;
+
+while (numeroDigitado < 0 || numeroDigitado > 9)
+{
+    Console.Write("Digite um número entre 1 e 9 (ou 0 para cancelar)...: ");
+    numeroDigitado = Convert.ToInt32(Console.ReadLine());
+}
+
+if (numeroDigitado == 0)
+{
+    Console.WriteLine("\nOperação cancelada.");
+}
+else
+{
+    Console.WriteLine($"\nNúmero selecionado = {numeroDigitado}");
+}
+
+Console.WriteLine("\nVolte Sempre!");
